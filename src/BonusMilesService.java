a35263c883179db4a9773a9d65226970dd195641
Mafka <kitm@digt.ru>
2023-05-07 19:56:32 +0300

    BonusMilesService

---
+++ src/BonusMilesService.java
@@ -0,0 +1,11 @@
+public class BonusMilesService {
+    public int calculate(int cost) {
+
+                int numberRublesOneMile = 20;
+                int price = 10000;
+
+                cost = ( price / numberRublesOneMile);
+
+                return cost;
+    }
+}
